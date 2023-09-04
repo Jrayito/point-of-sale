57798cedd4ca195d438d92abc2d0956b898dde41
Jesus Raymundo <jesusraymundi@gmail.com>
2023-09-04 16:05:17 -0600

    Se creo la vista de login

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,9 +1,55 @@
-import React from 'react';
-import '../assets/bootstrap/bootstrap.min.css';
+import {React, useState} from 'react';
+
+import InputCustom from '../componentes/InputCustom.js'
+import '../style/login.css';
 
 function Login() {
+
+   const [user, setUser] = useState("");
+   const [password, setPassword] = useState("");
+
+   const handleUser = (usuario) =>{
+      setUser(usuario);
+   }
+
+   const handlePassword = (password) =>{
+      setPassword(password)
+   }
+   
+   const handleSubmit = () => {}
+   
    return (
-         <h3>Hello World</h3>
-      );
+      <section className='container-login'>
+         <div className='card'>
+            <div className='card-body'>
+               <h4 className='text-primary mb-0'><b>¡Bienvenido!</b></h4>
+               <small>Escriba su usuario y contraseña para continuar.</small>
+               <div className='mt-5'>
+                  <InputCustom 
+                     type="text" 
+                     placeholder="Usuario:" 
+                     textLabel="Usuario:" 
+                     value={user} 
+                     onChange={handleUser}/>
+
+                  <InputCustom 
+                     type="password" 
+                     placeholder="Contraseña:" 
+                     textLabel="Contraseña:" 
+                     value={password} 
+                     onChange={handlePassword}/>
+
+                  <div className='d-grid gap-2'>
+                     <button
+                        onClick={handleSubmit}
+                        className='btn btn-primary mt-3'>
+                        Iniciar Sesión
+                     </button>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+   );
 }
 export default Login
