b135023dbd028101c889f2197666138b3ee158ea
Jesus Raymundo <jesusraymundi@gmail.com>
2023-09-05 14:29:56 -0600

    Se complemento la funcion de validaacion de usuaario

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,10 +1,13 @@
 import {React, useState} from 'react';
-
 import InputCustom from '../componentes/InputCustom.js'
 import '../style/login.css';
+// import {useNavigate} from 'react-router-dom';
+import {signInWithEmailAndPassword} from "firebase/auth";
+import {auth} from "../controllers/firebase.js"
 
 function Login() {
-
+   // const navigate = useNavigate();
+   const [isLogin, setIsLogin] = useState(true);
    const [user, setUser] = useState("");
    const [password, setPassword] = useState("");
 
@@ -16,14 +19,29 @@ function Login() {
       setPassword(password)
    }
    
-   const handleSubmit = () => {}
+   const handleSubmit = () => {
+      signInWithEmailAndPassword(auth, user, password)
+         .then((userCredential) => {})
+         .catch((error) =>{
+            setIsLogin(false);
+         });
+   }
    
    return (
       <section className='container-login'>
          <div className='card'>
             <div className='card-body'>
                <h4 className='text-primary mb-0'><b>¡Bienvenido!</b></h4>
-               <small>Escriba su usuario y contraseña para continuar.</small>
+                  {
+                     !isLogin 
+                        ? <small className="text-danger">
+                              <span className='d-flex align-items-center'>
+                                 <i className="material-icons md-18 me-1">error</i>
+                                 Usuario y/o contraseña incorrecta.
+                              </span>
+                           </small> 
+                        : <small >Escriba su usuario y contraseña para continuar.</small> 
+                  }
                <div className='mt-5'>
                   <InputCustom 
                      type="text" 
@@ -43,7 +61,9 @@ function Login() {
                      <button
                         onClick={handleSubmit}
                         className='btn btn-primary mt-3'>
-                        Iniciar Sesión
+                           <span className="d-flex align-items-center justify-content-center">
+                              Iniciar Sesión
+                           </span>
                      </button>
                   </div>
                </div>
