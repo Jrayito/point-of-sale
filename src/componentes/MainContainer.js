ef1020edf70f5241eddb7223c6a0939e615e7143
Jesus Raymundo <jesusraymundi@gmail.com>
2023-09-08 15:51:20 -0600

    Modificacion de vista del componente

--- src/componentes/MainContainer.js
+++ src/componentes/MainContainer.js
@@ -1,15 +1,3 @@
-import { Icon } from './Icon.js'
-export const MainContainer = ({ onClickChangeCollapse, collapsed }) => {
-
-   return (
-      <main style={{ padding: 10 }}>
-         <div>
-            <button className="btn btn-light" onClick={() => onClickChangeCollapse(!collapsed)}>
-               <span className="d-flex aling-items-center">
-                  <Icon name="menu" />
-               </span>
-            </button>
-         </div>
-      </main>
-   );
-}
+export const MainContainer = ({ children }) => {
+  return <main className="flex-grow-1 p-3">{children}</main>;
+};
