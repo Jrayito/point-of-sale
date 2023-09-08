b8e210dbae38e8485a433729fc4b03bdb1bfd6de
Jesus Raymundo <jesusraymundi@gmail.com>
2023-09-08 09:57:59 -0600

    Modificación de toda la estructura de login

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,8 +1,8 @@
 import { React, useState } from 'react';
-import InputCustom from '../componentes/InputCustom.js'
 import '../style/login.css';
 import { useNavigate } from 'react-router-dom'
 import { useAuth } from '../context/authContext.js';
+import {Banner} from '../componentes/Banner.js'
 
 export const Login = () => {
    const { login } = useAuth();
@@ -30,47 +30,40 @@ export const Login = () => {
       }
    }
 
+
    return (
-      <section className='container-login'>
-         <div className='card'>
-            <div className='card-body'>
-               <h4 className='text-primary mb-0'><b>¡Bienvenido!</b></h4>
-               {error
-                  ? <small className="text-danger">
-                     <span className='d-flex align-items-center'>
-                        <i className="material-icons md-18 me-1">error</i>
-                        Usuario y/o contraseña incorrecta.
-                     </span>
-                  </small>
-                  : <small >Escriba su usuario y contraseña para continuar.</small>
-               }
-               <div className='mt-5'>
-                  <InputCustom
-                     type="text"
-                     placeholder="Usuario:"
-                     textLabel="Usuario:"
-                     value={user}
-                     onChange={handleUser} />
-
-                  <InputCustom
-                     type="password"
-                     placeholder="Contraseña:"
-                     textLabel="Contraseña:"
-                     value={password}
-                     onChange={handlePassword} />
-
-                  <div className='d-grid gap-2'>
-                     <button
-                        onClick={handleSubmit}
-                        className='btn btn-primary mt-3'>
-                        <span className="d-flex align-items-center justify-content-center">
-                           Iniciar Sesión
-                        </span>
-                     </button>
+      <div className="container-fluid h-100">
+         <div className="row h-100">
+            <div className="col-md-8 d-none d-md-block gradient d-flex flex-column justify-content-center align-items-center">
+               <div className="container-logan">
+                  <div>
+                     <Banner subtitleClass="text-white"/>
+                     {/* <h2 className='text-white d-block text-start name'><b>BizTrack</b></h2>
+                     <span className='d-block text-white text-start subtitle'>Ingresa al centro de operaciones: <strong>tu negocio, tu control.</strong></span> */}
                   </div>
                </div>
             </div>
+            <div className="col-md-4 col-12 d-flex align-items-center">
+               <div className="container p-5">
+                  <Banner titleClass="name-form" subtitleClass="text-black subtitle-form" />
+                  {/* <h2 className='text-white d-block text-start name name-form'><b>BizTrack</b></h2>
+                  <span className='d-block text-black text-start subtitle subtitle-form'>Ingresa al centro de operaciones: <strong>tu negocio, tu control.</strong></span> */}
+                  <h3 className='mt-5'>¡Bienvenido!</h3>
+                  <span>Escriba su usuario y contraseña.</span>
+                  <form className='mt-4'>
+                     <div className="form-group">
+                        <label htmlFor="email">Correo electrónico</label>
+                        <input type="email" id="email" name="email" className="form-control" />
+                     </div>
+                     <div className="form-group mt-3">
+                        <label htmlFor="password">Contraseña</label>
+                        <input type="password" id="password" name="password" className="form-control" />
+                     </div>
+                     <button type="submit" className="btn btn-primary mt-3">Iniciar sesión</button>
+                  </form>
+               </div>
+            </div>
          </div>
-      </section>
+      </div>
    );
 }
