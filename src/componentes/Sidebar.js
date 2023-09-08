8189bddc4db3edb43bebab6f0dcc091678a69e90
Jesus Raymundo <jesusraymundi@gmail.com>
2023-09-08 15:50:27 -0600

    Formateo de documento

--- src/componentes/Sidebar.js
+++ src/componentes/Sidebar.js
@@ -1,32 +1,26 @@
-import { useState } from 'react';
 import { Sidebar as Bar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
-import { Icon } from '../componentes/Icon.js';
-import { Link } from 'react-router-dom';
-import { MainContainer } from './MainContainer.js';
-import { SideBarHeader } from './SideBarHeader.js';
-import '../style/sidebar.css';
+import { Icon } from "../componentes/Icon.js";
+import { Link } from "react-router-dom";
+import { SideBarHeader } from "./SideBarHeader.js";
+import "../style/sidebar.css";
+import { useSidebar } from "../context/sidebarContext.js";
 
 export const Sidebar = () => {
-   const [collapsed, setCollapsed] = useState(false);
+  const { collapsed } = useSidebar();
 
-   const changeCollapse = (collapsed) => {
-      setCollapsed(collapsed);
-   }
-
-   return (
-      <div className='container-sidebar'>
-         <Bar collapsed={collapsed}>
-            <div className='container-menu-sidebar'>
-               <SideBarHeader isCollpase={collapsed}/>
-               <Menu>
-                  <MenuItem icon={<Icon name="home" />} component={<Link to="/" />}>Dashboard</MenuItem>
-                  <SubMenu icon={<Icon name="shopping_cart"/>} label="Compras">
-                     <MenuItem  component={<Link to="/" />}>Productos</MenuItem>
-                  </SubMenu>
-               </Menu>
-            </div>
-         </Bar>
-         <MainContainer onClickChangeCollapse={changeCollapse} collapsed={collapsed} />
+  return (
+    <Bar collapsed={collapsed}>
+      <div className="container-menu-sidebar">
+        <SideBarHeader isCollpase={collapsed} />
+        <Menu>
+          <MenuItem icon={<Icon name="home" />} component={<Link to="/" />}>
+            Dashboard
+          </MenuItem>
+          <SubMenu icon={<Icon name="shopping_cart" />} label="Compras">
+            <MenuItem component={<Link to="/productos" />}>Productos</MenuItem>
+          </SubMenu>
+        </Menu>
       </div>
-   )
-}
+    </Bar>
+  );
+};
