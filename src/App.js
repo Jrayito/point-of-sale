ee2a7692e05eb1de8df46fe5791e073bc6df835a
Jesus Raymundo <jesusraymundi@gmail.com>
2023-09-08 15:46:43 -0600

    Formateo del documento y se agrego nueva ruta productos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import './assets/bootstrap/bootstrap.min.css';
-import { Routes, Route } from 'react-router-dom';
-import { Login } from './views/Login.js';
-import { AuthProvider } from './context/authContext.js';
-import { ProtectedRoute } from './componentes/ProtectedRoute.js';
-import { Dashboard } from './views/Dashboard.js';
-import './style/button.css';
+import React from "react";
+import "./assets/bootstrap/bootstrap.min.css";
+import { Routes, Route } from "react-router-dom";
+import { Login } from "./views/Login.js";
+import { AuthProvider } from "./context/authContext.js";
+import { SidebarProvider } from "./context/sidebarContext.js";
+import { ProtectedRoute } from "./componentes/ProtectedRoute.js";
+import { Dashboard } from "./views/Dashboard.js";
+import { Productos } from "./views/Productos.js";
+import "./style/reset.css";
 
 function App() {
   return (
     <div className="vh-100 bg-body-tertiary">
       <AuthProvider>
-        <Routes>
-          <Route path="/" element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          } />
-          <Route path="/login" element={<Login />} />
-        </Routes>
+        <SidebarProvider>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <ProtectedRoute>
+                  <Dashboard />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/productos"
+              element={
+                <ProtectedRoute>
+                  <Productos />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </SidebarProvider>
       </AuthProvider>
     </div>
   );
