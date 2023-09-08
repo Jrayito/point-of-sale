f1d59ac80aeafa29a5ba3d1534b9a6b5c6409a8c
Jesus Raymundo <jesusraymundi@gmail.com>
2023-09-08 10:55:32 -0600

    Se agrego el typewrite para animación del banner

--- src/componentes/Banner.js
+++ src/componentes/Banner.js
@@ -1,14 +1,39 @@
 import React from 'react';
+import { useTypewriter, Cursor } from 'react-simple-typewriter';
 
-export const Banner = ({titleClass, subtitleClass}) => {
-   
-   const titleAsigneClass = "text-white d-block text-start name";
-   const subtitleAsignedClass = "d-block text-start subtitle"
+export const Banner = ({ titleClass, subtitleClass, isAnimation }) => {
+   const words = [
+      " Tu negocio.",
+      " Tu control.",
+      " Toma el mando."
+   ];
+
+   const [text] = useTypewriter({
+      words: words,
+      loop: 0,
+      typeSpeed: 50,
+      deleteSpeed: 30,
+      enable: false
+   });
 
    return (
       <>
-         <h2 className={`${titleAsigneClass} ${titleClass}`}><b>BizTrack</b></h2>
-         <span className={`${subtitleAsignedClass} ${subtitleClass}`}>Ingresa al centro de operaciones: <strong>tu negocio, tu control.</strong></span>
+         <h2 className={`text-white d-block text-start name ${titleClass ?? ""}`}><b>BizTrack</b></h2>
+         <span className={`d-block text-start subtitle ${subtitleClass ?? ""}`}>
+            {isAnimation
+               ? "La puerta a la gestión eficiente de tu negocio se abre aquí."
+               : "Ingresa al centro de operaciones: tu negocio, tu control."}
+            <strong>
+               {isAnimation ? text : ""}
+            </strong>
+            {
+               isAnimation 
+               ? <span>
+                  <Cursor />
+               </span> 
+               : <span></span>
+            }
+         </span>
       </>
    )
 }
