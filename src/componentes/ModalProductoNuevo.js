8d204c4f3237af64ea0f9e23f5967c1f142c4090
Jesus Raymundo <jesusraymundi@gmail.com>
2023-09-12 09:43:06 -0600

    Se empezo a agregar las funciones para guardar el producto

--- src/componentes/ModalProductoNuevo.js
+++ src/componentes/ModalProductoNuevo.js
@@ -3,12 +3,33 @@ import { BottonGuardar } from "./buttons/ButtonGuardar";
 import { CustomInput } from "./inputs/CustomInput";
 import { SelectUnidadMedida } from "./selects/UnidadMedida";
 import { InputMoney } from "./inputs/inputMoney.js";
+import { CustonInputNumber } from "./inputs/CustonInputNumber.js";
+import { addProduct } from "../controllers/productos.js";
 
 export const ModalProductoNuevo = () => {
-  const [money, setMoney] = useState(0);
-  const handleGetMoney = (money) => {
-    console.log(money);
+  const [product, setProduct] = useState({
+    sku: "",
+    description: "",
+    unidadMedida: {},
+    precioCompra: 0,
+    precioVenta: 0,
+    stock: 0,
+    minStock: 0,
+  });
+
+  const handleAddInformacion = (name, value) => {
+    setProduct({ ...product, [name]: value });
+    console.log(product);
   };
+
+  const handleAddProduct = async () => {
+    try {
+      await addProduct(product);
+    } catch (error) {
+      console.log("Ocurrio un error:" + error);
+    }
+  };
+
   return (
     <div
       className="modal fade"
@@ -33,28 +54,56 @@ export const ModalProductoNuevo = () => {
             <h6 className="mb-2">Informacion general:</h6>
             <div className="row">
               <CustomInput
-                descripcion="SKU"
+                descripcion="Código o SKU:"
                 inputID="inputSKU"
                 placeholder="00000000"
+                getInformaction={handleAddInformacion}
+                name="sku"
               />
               <CustomInput
-                descripcion="Descripción"
+                descripcion="Descripción:"
                 inputID="inputDescripcion"
                 placeholder="Descripción del producto"
                 col="col-md-9"
+                getInformaction={handleAddInformacion}
+                name="description"
               />
-              <SelectUnidadMedida selectID="selectUnidadMedida" />
+              <SelectUnidadMedida
+                selectID="selectUnidadMedida"
+                getInformaction={handleAddInformacion}
+                name="unidadMedida"
+              />
               <InputMoney
-                money={money}
-                setMoney={handleGetMoney}
+                money={product.precioCompra}
+                getMoney={handleAddInformacion}
                 inputID={"002"}
                 description="Precio de compra:"
+                name="precioCompra"
               />
               <InputMoney
-                money={money}
-                setMoney={handleGetMoney}
+                money={product.precioVenta}
+                getMoney={handleAddInformacion}
                 inputID={"002"}
                 description="Precio de venta:"
+                name="precioVenta"
+              />
+              <CustonInputNumber
+                number={product.stock}
+                getNumber={handleAddInformacion}
+                inputID={"003"}
+                descripcion="Stock:"
+                name="stock"
+              />
+            </div>
+
+            <h6 className="mt-4">Informacion opcional:</h6>
+            <div className="row">
+              <CustonInputNumber
+                number={product.minStock}
+                getNumber={handleAddInformacion}
+                inputID={"004"}
+                descripcion="Stock mínimo:"
+                name="minStock"
               />
             </div>
           </div>
@@ -66,7 +115,7 @@ export const ModalProductoNuevo = () => {
             >
               Cancelar
             </button>
-            <BottonGuardar />
+            <BottonGuardar handleGuardar={handleAddProduct} />
           </div>
         </div>
       </div>
