daa7232b0a381c74d2921ab6104209ba03e381b8
Jesus Raymundo <jesusraymundi@gmail.com>
2023-09-13 10:12:59 -0600

    Se agrego un nuevo botton para toggle

--- src/componentes/MainHeader.js
+++ src/componentes/MainHeader.js
@@ -2,18 +2,29 @@ import { Icon } from "./Icon.js";
 import { useSidebar } from "../context/sidebarContext.js";
 
 export const MainHeader = ({ children }) => {
-  const { collapsed, changeCollapse } = useSidebar();
+  const { collapsed, changeCollapse, changeToggle, toggle, broken } =
+    useSidebar();
 
   return (
     <div className="d-flex align-items-center justify-content-between flex-grow-1">
-      <button
-        className="btn btn-light"
-        onClick={() => changeCollapse(!collapsed)}
-      >
-        <span className="d-flex aling-items-center">
-          <Icon name="menu" />
-        </span>
-      </button>
+      {!broken && (
+        <button
+          className="btn btn-light"
+          onClick={() => changeCollapse(!collapsed)}
+        >
+          <span className="d-flex aling-items-center">
+            <Icon name="menu" />
+          </span>
+        </button>
+      )}
+      {broken && (
+        <button className="btn btn-light" onClick={() => changeToggle(!toggle)}>
+          <span className="d-flex aling-items-center">
+            <Icon name="menu" />
+          </span>
+        </button>
+      )}
+
       <div>{children}</div>
     </div>
   );
