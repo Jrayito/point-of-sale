460fedd02a8a179136c0035834365216419b47cd
Jesus Raymundo <jesusraymundi@gmail.com>
2023-09-20 12:22:16 -0600

    Se agrego la funcion para recuperar la informacion de un producto

--- src/controllers/productos.js
+++ src/controllers/productos.js
@@ -1,5 +1,7 @@
 import { db } from "./firebase.js";
-import { collection, addDoc, getDocs } from "firebase/firestore";
+import { collection, addDoc, getDocs, doc, getDoc } from "firebase/firestore";
+
+export const acciones = [];
 
 export const addProduct = async (product) => {
   await addDoc(collection(db, "products"), product);
@@ -8,4 +10,10 @@ export const addProduct = async (product) => {
 export const getProducts = async () => {
   const querySnapshot = await getDocs(collection(db, "products"));
   return querySnapshot;
+};
+
+export const getProduct = async (docID) => {
+  const docRef = doc(db, "products", docID);
+  const docSnap = await getDoc(docRef);
+  return docSnap.data();
 };
